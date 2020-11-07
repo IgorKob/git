7e6d8a9ebbbfea371de06111a714165705a5072b
IgorKob <kobrynovych@gmail.com>
2020-11-07 21:41:27 +0200

    promise -> asinc/await

--- js/Promise.js
+++ js/Promise.js
@@ -17,3 +17,29 @@ sleep(5000).then(() => console.log('5s'))
   Promise.race([sleep(2000), sleep(5000)]).then(() => {
     console.log('Проміси будуть виконуватися попорядку')
   })
+
+
+// // async/await
+// //login
+// export const Loginkim = (email, password, rememberMe) => (dispatch) => {
+//
+//   authAPI.login(email, password, rememberMe)
+//     .then((response) => {
+//       if (response.data.resultCode === 0) {
+//         // let {userId, email, login} = data.data;
+//         dispatch(getAuthUserData());
+//       } else {
+//         let message = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error';
+//         dispatch(stopSubmit('login',{_error: message}));
+//       }
+//     });
+// }
+export const Loginkim = (email, password, rememberMe) => async (dispatch) => {
+ let response = await authAPI.login(email, password, rememberMe);
+      if (response.data.resultCode === 0) {
+        dispatch(getAuthUserData());
+      } else {
+        let message = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error';
+        dispatch(stopSubmit('login',{_error: message}));
+      }
+}
