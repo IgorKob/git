7b6ed38adf2c84613f925dd79662251bfd28c660
IgorKob <kobrynovych@gmail.com>
2020-12-07 19:56:53 +0200

    react, lodash lib

--- practice/React_shop_cart_search_filter/src/Components/Spinner/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Spinner.css'
-
-export default function Spinner() {
-    return (
-        <div class="wrapp"><div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>
-    )
-}
