d2253534c7029b1af49724731cf0f9346e37ccf5
IgorKob <kobrynovych@gmail.com>
2020-12-14 23:45:34 +0200

    react: github api

---
+++ React/React_GitHub_API/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GitHubAPI from './Components/GitHubAPI/GitHubAPI';
+
+function App() {
+  return (
+    <>
+      <GitHubAPI />    
+    </>
+  );
+}
+
+export default App;
