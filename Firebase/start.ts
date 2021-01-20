8b195faaab1f3b4838fd15daf02a9280e1e87c84
IgorKob <kobrynovych@gmail.com>
2021-01-20 02:12:17 +0200

    start Firebase...

---
+++ Firebase/start.ts
@@ -0,0 +1,15 @@
+// https://firebase.google.com/
+// https://www.robinwieruch.de/complete-firebase-authentication-react-tutorial
+
+// npm install firebase
+
+// npm install -g firebase-tools
+
+// Увійдіть в обліковий запис Google
+firebase login
+
+// ініціюйте проект
+firebase init
+
+// розгорніть додаток
+firebase deploy
